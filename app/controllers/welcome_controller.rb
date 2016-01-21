80e5269e72f190e1ad18a4d7043de73d9218a708
Oleg Bloshenkin <darelvc@gmail.com>
2016-01-21 05:43:55 +0000

    Fix bug deploy on heroku 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,4 @@
 class WelcomeController < ApplicationController
-<<<<<<< HEAD
   def index
   end
-=======
-	def index
-		
-	end
->>>>>>> 161f994d2cda39520c5f44efed35f46e51e4e5b2
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
 Rails.application.config.session_store :cookie_store, key: '_workspace_session'
-=======
-Rails.application.config.session_store :cookie_store, key: '_chat_session'
->>>>>>> 161f994d2cda39520c5f44efed35f46e51e4e5b2
