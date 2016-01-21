c97f7b66f1b2dbfc08019e9833ab65295edd6aca
Oleg Bloshenkin <darelvc@gmail.com>
2016-01-21 05:38:54 +0000

    Fix bug deploy on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
   get 'welcome/index'
 
@@ -8,9 +7,4 @@ Rails.application.routes.draw do
   resources 'chats'
   
   get '*path' => redirect('/')
-=======
-  get 'welcome/index'
-
-  root 'welcome#index'
->>>>>>> 161f994d2cda39520c5f44efed35f46e51e4e5b2
 end
