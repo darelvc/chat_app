1bd98e676d87a8a0fae70859f2e258787d4d1d07
Oleg Bloshenkin <darelvc@gmail.com>
2016-01-21 12:36:45 +0000

    Add image to chat messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 		image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
+	
+	def link_to_in_li(body, url)
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url
+    end
+  end
+
 end
